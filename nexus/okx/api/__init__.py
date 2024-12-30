2be3fc1d4084dcb11a49aec68d8ac5cd9f7c61e4
velit <2476358005@qq.com>
2024-12-30 11:19:51 +0800

    feat: CopyTradingApi Implementation

--- nexus/okx/api/__init__.py
+++ nexus/okx/api/__init__.py
@@ -3,3 +3,4 @@ from .algo_trading import AlgoTradingApi
 from .grid_trading import GridTradingApi
 from .signal_bot_trading import SignalBotTradingApi
 from .recurring_buy import RecurringBuyApi
+from .copy_trading import CopyTradingApi
